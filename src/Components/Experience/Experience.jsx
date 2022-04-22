4114f03601d53f9687758389c2092118c50e6e75
Mukesh Pathak <80037766+developermickey@users.noreply.github.com>
2022-04-23 02:18:04 +0530

    Added Experience Section

---
+++ src/Components/Experience/Experience.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Experience.css'
+
+const Experience = () => {
+  return (
+    <div className='experience'>
+      <div className='achievement'>
+          <div className="circle">
+              8+
+          </div>
+          <span>Years</span>
+          <span>Experience</span>
+      </div>
+      <div className='achievement'>
+          <div className="circle">
+              200+
+          </div>
+          <span>Completed</span>
+          <span>Projects</span>
+      </div>
+      <div className='achievement'>
+          <div className="circle">
+              5+
+          </div>
+          <span>Companies</span>
+          <span>Work</span>
+      </div>
+
+    </div>
+  )
+}
+
+export default Experience
